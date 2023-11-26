ce759f811e3f75e3a9087b2c79bb97c6b3f0fb80
Miguel Michel <miguelmichel@gatech.edu>
2023-11-26 16:55:59 -0600

    Implemented pdf_to_csv_embeddings script

--- app/assets/scripts/pdf_to_csv_embeddings.rb
+++ app/assets/scripts/pdf_to_csv_embeddings.rb
@@ -1,15 +1,41 @@
 require 'optparse'
 require 'pdf/reader'
-require "baran"
+require 'csv'
+require 'baran'
+require 'uri'
+require 'net/http'
+require 'json'
+
+# Define constants
+OPENAI_EMBEDDING_URI = "https://api.openai.com/v1/embeddings"
+OPENAI_EMBEDDING_MODEL = "text-embedding-ada-002"
 
 # Initialize variables
 key = ''
 pdf_path = ''
-csv_path = 'embeddings.csv'
+csv_path = 'pdf_embeddings.csv'
 
+def get_openai_embeddings(text, key)
+    uri = URI(OPENAI_EMBEDDING_URI)
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+
+    body = {}
+    body['input'] = text
+    body['model'] = OPENAI_EMBEDDING_MODEL
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request['Authorization'] = "Bearer " + key
+    request['Content-Type'] = 'application/json'
+
+    request.body = body.to_json
+    response = https.request(request)
+
+    JSON.parse(response.body)
+end
 
 # Define option parser and create instance
-optparse = OptionParser.new do |parser|
+OptionParser.new do |parser|
   parser.banner = "Usage: pdf_to_csv_embeddings.rb [options]"
 
   parser.on("-k", "--key KEY", "OpenAI secret key") { |v| key = v }
@@ -32,12 +58,12 @@ end
 
 # Read pdf pages, for each page, generate OpenAI embedding and save in csv file
 reader = PDF::Reader.new(pdf_path)
-text_to_embed = ''
+pdf_text = ''
 previous_page_context = ''
 
 reader.pages.each do |page|
     # Extract pdf text and remove page numbers
-    text_to_embed = text_to_embed + page.text.gsub(/\n|\s+(\d+)/,' ')
+    pdf_text = pdf_text + page.text.gsub(/\n|\s+(\d+)/,' ')
 end
 
 splitter = Baran::RecursiveCharacterTextSplitter.new(
@@ -46,8 +72,18 @@ splitter = Baran::RecursiveCharacterTextSplitter.new(
     separators: ["\"", "\n", " ", ""]
 )
 
-chunks = splitter.chunks(text_to_embed)
+chunks = splitter.chunks(pdf_text)
 
-chunks.each do |chunks|
-    # Call open AI api
+# Open or create csv file
+CSV.open(csv_path, 'w') do |csv|
+    # Write headers
+    csv << ['text', 'embeddings']
+
+    chunks.each do |chunk|
+        # Call open AI api to get embeddings
+        response = get_openai_embeddings(chunks[0][:text], key)
+    
+        # Save in csv file
+        csv << [chunk[:text], response["data"][0]["embedding"].join(',')]
+    end
 end
