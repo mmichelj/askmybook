f5c66ac6fb041cfd5d8a6affd6ecb02d8a44456b
Miguel Michel <miguelmichel@gatech.edu>
2023-11-27 15:52:25 -0600

    Set database up

---
+++ db/migrate/20231127030735_create_question_answers.rb
@@ -0,0 +1,11 @@
+class CreateQuestionAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :question_answers, id: :uuid do |t|
+      t.timestamps null: false, default: -> { 'NOW()' }, index: true
+
+      t.string :answer, null: false
+      t.vector :question_embedding, dimensions: 1536, using: :ivfflat, opclass: :vector_ip_ops
+      t.integer :token_count, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_27_030735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "vector"
+
+  create_table "question_answers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.string "answer", null: false
+    t.vector "question_embedding"
+    t.integer "token_count", null: false
+    t.index ["created_at"], name: "index_question_answers_on_created_at"
+    t.index ["updated_at"], name: "index_question_answers_on_updated_at"
+  end
 
 end
