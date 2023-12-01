367cee1a1f23516e144a4d08c68d8eb89b7cb958
Miguel Michel <miguelmichel@gatech.edu>
2023-11-30 21:12:00 -0600

    Skipping non required db migrations

--- db/migrate/20231130213432_add_default_false_to_known_for_question_answer.rb
+++ db/migrate/20231130213432_add_default_false_to_known_for_question_answer.rb
@@ -1,5 +1,5 @@
 class AddDefaultFalseToKnownForQuestionAnswer < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :question_answers, :known, from: nil, to: false
+    #change_column_default :question_answers, :known, from: nil, to: false
   end
 end
