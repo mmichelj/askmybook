d31bbe7318072232a29ceb9321f44fb90b348005
Miguel Michel <48222418+mmichelj@users.noreply.github.com>
2023-11-30 21:22:16 -0600

    Fix/cleanup (#7)
    * Cleaning up the back end
    
    * Cleaning up UI
    
    * Changes for deployment in Heroku
    
    * Setting up db configs
    
    * Skipping non required db migrations

--- db/migrate/20231130213331_add_known_to_question_answers.rb
+++ db/migrate/20231130213331_add_known_to_question_answers.rb
@@ -1,5 +1,5 @@
 class AddKnownToQuestionAnswers < ActiveRecord::Migration[7.0]
   def change
-    add_column :question_answers, :known, :boolean
+    #add_column :question_answers, :known, :boolean
   end
 end
