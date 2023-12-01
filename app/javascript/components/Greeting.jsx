f1e799cb65806fb371faf54200b33ae9126b3966
Miguel Michel <miguelmichel@gatech.edu>
2023-11-30 23:29:44 -0600

    Added cover

--- app/javascript/components/Greeting.jsx
+++ app/javascript/components/Greeting.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Image from "../../../public/cover.jpg";
 
 const Greeting = () => {
     
     return (
         <React.Fragment>
-            <img src={Image} className="rounded mx-auto mb-4" alt="Book cover" />
+            <img src="https://images.booksense.com/images/512/282/9781788282512.jpg" className="rounded mx-auto mb-4" alt="Book cover" />
             <h1 className="display-4">Ask Sherlock Holmes</h1>
             <p className="lead">Ask anything about <em>The Adventures of Sherlock Holmes</em> by Arthur Conan Doyle.</p>
             <hr className="my-4" />
